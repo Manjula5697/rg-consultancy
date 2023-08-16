8b4d39c91fbb6f8e3951b633ae3a6bd560d1449a
mk00818823 <manjula.kalyan@borngroup.com>
2023-08-16 13:16:25 +0530

    site-relanuch

---
+++ js/init.js
@@ -0,0 +1,21 @@
+/*-----------------------------------------------------------------------------------
+/*
+/* Init JS
+/*
+-----------------------------------------------------------------------------------*/
+
+jQuery(document).ready(function($) {
+
+    /*---------------------------------------------------- */
+    /* Preloader
+    ------------------------------------------------------ */ 
+          $(window).load(function() {
+    
+           // will first fade out the loading animation 
+            $("#status").fadeOut("slow"); 
+    
+            // will fade out the whole DIV that covers the website. 
+            $("#preloader").delay(500).fadeOut("slow").remove();      
+    
+          })
+});
